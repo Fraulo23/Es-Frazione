a506918daac282a1c207d1a1fe4d01dac556e443
Fraulo23 <fraulinifederico@gmail.com>
2024-01-30 16:19:17 +0100

    Aggiungere i file di progetto.

---
+++ Frazione.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Frazione
+{
+    internal class Frazione
+    {
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+        public Frazione(int numeratore, int denominatore)
+        {
+            Numeratore = numeratore;
+            Denominatore = denominatore;
+        }
+        public int MCD()
+        {
+            int num = Numeratore;
+            int den=Denominatore;
+            while (num > 0)
+            {
+                if (num == den || num == 0 || den == 0)
+                {
+                    return num;
+                }
+                if (num > den)
+                {
+                    num -= den;
+                }
+                else
+                {
+                    int tmp = den;
+                    den = num;
+                    num = tmp;
+                }
+            }
+            return den;
+        }
+        public void Semplifica()
+        {
+            int mcd = MCD();
+            Numeratore = Numeratore / mcd;
+            Denominatore=Denominatore/ mcd;
+        }
+        public override string ToString()
+        {
+            Semplifica();
+            return Numeratore + "/" + Denominatore;
+        }
+        public static Frazione Parse(string s)
+        {
+            string[] frazione=s.Split('/');
+            int num = int.Parse(frazione[0]);
+            int den = int.Parse(frazione[1]);
+            return new Frazione(num, den);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione f = new Frazione(16, 32);
+            Frazione f2=Frazione.Parse(f.ToString());
+            Console.WriteLine(f2);
+            Console.ReadLine();
+        }
+    }
+}
